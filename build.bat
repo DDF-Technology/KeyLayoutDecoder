5f754c6aa460b79d68e8765b8febbacc4ad49fc5
Fabio De Deo <fabio.dedeo@gmail.com>
2026-08-25 19:18:33 +0200

    Fix MSVC discovery in CI

--- build.bat
+++ build.bat
@@ -1,11 +1,16 @@
 @echo off
-setlocal EnableDelayedExpansion
+setlocal EnableExtensions
 where cl >nul 2>nul
-if errorlevel 1 (
-  for %%E in (Community Professional Enterprise BuildTools) do if exist "%ProgramFiles%\Microsoft Visual Studio\2022\%%E\VC\Auxiliary\Build\vcvars64.bat" set "VSROOT=%ProgramFiles%\Microsoft Visual Studio\2022\%%E"
-  if not defined VSROOT goto :no_compiler
-  call "!VSROOT!\VC\Auxiliary\Build\vcvars64.bat" >nul
-)
+if not errorlevel 1 goto :build
+
+set "VSWHERE=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
+if not exist "%VSWHERE%" goto :no_compiler
+for /f "usebackq delims=" %%I in (`"%VSWHERE%" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do set "VSROOT=%%I"
+if not defined VSROOT goto :no_compiler
+call "%VSROOT%\VC\Auxiliary\Build\vcvars64.bat" >nul
+if errorlevel 1 goto :no_compiler
+
+:build
 cl /nologo /W4 /O2 /MT key_layout_decoder.c /Fe:KeyLayoutDecoder.exe /link /SUBSYSTEM:WINDOWS user32.lib shell32.lib
 if errorlevel 1 exit /b 1
 echo Creato KeyLayoutDecoder.exe
